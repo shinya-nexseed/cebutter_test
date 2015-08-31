2012544ea8789444fe095d42e80b43f698b0842d
shinyahirai <908.shinya@gmail.com>
2015-08-31 23:23:15 +0800

    ログイン処理の画面ひな形作成

---
+++ join/check.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Cebutter - 会員登録</title>
+</head>
+<body>
+  <h1>会員登録</h1>
+  <p>記入した内容を確認して、「登録する」ボタンをクリックしてください</p>
+  <form action="" method="post">
+    <dl>
+      <dt>ニックネーム</dt>
+      <dd>
+      </dd>
+
+      <dt>メールアドレス</dt>
+      <dd>
+      </dd>
+
+      <dt>パスワード</dt>
+      <dd>
+      【表示されません】
+      </dd>
+
+      <dt>写真など</dt>
+      <dd>
+      </dd>
+    </dl>
+    <div><a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a> | <input type="submit" value="登録する" /></div>
+  </form>
+</body>
+</html>
---
+++ join/thanks.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Cebutter - 会員登録</title>
+</head>
+<body>
+  <h1>会員登録</h1>
+  <p>ユーザー登録が完了しました</p>
+  <p><a href="../">ログインする</a></p>
+</body>
+</html>
